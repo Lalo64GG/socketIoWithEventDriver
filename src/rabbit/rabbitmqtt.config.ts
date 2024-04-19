0561bda399de940cf961e186a21ee7358071c74c
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-04-19 09:50:53 -0600

    Add initial files and configurations

---
+++ src/rabbit/rabbitmqtt.config.ts
@@ -0,0 +1,23 @@
+import amqp from 'amqplib';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    protocol: "amqp",
+    hostname: "54.210.113.84",
+    port: 5672,
+    username: "guest",
+    password: "guest",
+  };
+
+export async function connectToRabbitMQ() {
+    try {
+        const conn = await amqp.connect(config);
+        console.log('Conexion a RabbitMQ exitosa');
+        return conn.createChannel(); 
+    } catch ( error ) {
+        console.log('Error al conectar con RabbitMQ', error);
+        throw error;
+    }
+}
