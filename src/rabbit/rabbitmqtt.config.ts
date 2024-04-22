03475e7fe6e8f0689fb522f6a97de4d3a5aa1f35
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-04-22 11:47:03 -0600

    Update RabbitMQ connection configuration in rabbitmqtt.config.ts

--- src/rabbit/rabbitmqtt.config.ts
+++ src/rabbit/rabbitmqtt.config.ts
@@ -1,23 +1,22 @@
-import amqp from 'amqplib';
-import dotenv from 'dotenv';
+import amqp from "amqplib";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const config = {
-    protocol: "amqp",
-    hostname: "3.228.161.219",
-    port: 5672,
-    username: "guest",
-    password: "guest",
-  };
+  protocol: "amqp",
+  hostname: "3.228.161.219",
+  username: "guest",
+  password: "guest",
+};
 
 export async function connectToRabbitMQ() {
-    try {
-        const conn = await amqp.connect(config);
-        console.log('Conexion a RabbitMQ exitosa');
-        return conn.createChannel(); 
-    } catch ( error ) {
-        console.log('Error al conectar con RabbitMQ', error);
-        throw error;
-    }
+  try {
+    const conn = await amqp.connect(config);
+    console.log("Conexion a RabbitMQ exitosa");
+    return conn.createChannel();
+  } catch (error) {
+    console.log("Error al conectar con RabbitMQ", error);
+    throw error;
+  }
 }
